cee90f26b15bf4a724c5a8f8a9bca872e5cf868a
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-13 11:38:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// variables
+
+let name = "Nihala nasreen AS";
+let age = 18;
+let course = "JavaScript";
+
+// console output
+
+console.log("Welcome to Java Script PLayground");
+
+// template literals
+ console.log(`My name is ${name}`);
+ console.log(`I am ${age} years old`);
+ console.log(`I am learning ${course}`);
+
+//  operators
+
+let a = 20;
+let b = 5;
+
+console.log(`Addition: ${a + b}`);
+console.log(`Substraction: ${a - b}`);
+console.log(`Multiplication: ${a * b}`);
+console.log(`Division: ${a / b}`);
+console.log(`Remainder: ${a % b}`);
