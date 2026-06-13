f6af4f59fb4ae33d31aad39284c529ae0a68bb40
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-13 11:49:26 +0530

    updated

--- app.js
+++ app.js
@@ -18,14 +18,28 @@ console.log('-----Student Profile------');
  console.log(`My name is ${name}`);
  console.log(`I am ${age} years old`);
  console.log(`I am learning ${course}`);
+ console.log(`I live in ${city}`);
 
 //  operators
 
 let a = 20;
 let b = 5;
 
+console.log("----- Arithmetic-----");
+
 console.log(`Addition: ${a + b}`);
 console.log(`Substraction: ${a - b}`);
 console.log(`Multiplication: ${a * b}`);
 console.log(`Division: ${a / b}`);
 console.log(`Remainder: ${a % b}`);
+
+// string 
+
+console.log("----- String-----");
+
+let firstName = "Nihala";
+let lastname = "Nasreen";
+
+let fullName = firstname + " " + lastName;
+
+
